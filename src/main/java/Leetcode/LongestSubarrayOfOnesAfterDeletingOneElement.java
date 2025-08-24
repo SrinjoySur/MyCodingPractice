c40858d0e44eca285c83db479f669b7f1af20543
Srinjoy Sur <srinjoysur@gmail.com>
2025-08-24 15:39:43 +0530

    feat: another leetcode answer

---
+++ src/main/java/Leetcode/LongestSubarrayOfOnesAfterDeletingOneElement.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestSubarrayOfOnesAfterDeletingOneElement {
+    public int longestSubarray(int[] nums) {
+        int i=0,j=0,n=nums.length;
+        int maxi=0,count=0;
+        while(j<n){
+            if(nums[j]==0){
+                count++;
+            }
+            while(count>1){
+                if(nums[i]==0){
+                    count--;
+                }
+                i++;
+            }
+            maxi=Math.max(maxi,j-i);
+            j++;
+        }
+
+        return maxi;
+    }
+}
--- src/main/java/Leetcode/Main.java
+++ src/main/java/Leetcode/Main.java
@@ -9,8 +9,12 @@ public class Main{
 //        FourSum fourSum=new FourSum();
 //        System.out.println(fourSum.fourSum(new int[]{1,0,-1,0,-2,2},0));
 //        System.out.println(fourSum.fourSum(new int[]{2,2,2,2,2},8));
-            FindTheMinimumAreaToCoverAllOnes sol=new FindTheMinimumAreaToCoverAllOnes();
-            System.out.println(sol.minimumArea(new int[][]{{0,1,0},{1,0,1}}));
-            System.out.println(sol.minimumArea(new int[][]{{1,0},{0,0}}));
+//            FindTheMinimumAreaToCoverAllOnes sol=new FindTheMinimumAreaToCoverAllOnes();
+//            System.out.println(sol.minimumArea(new int[][]{{0,1,0},{1,0,1}}));
+//            System.out.println(sol.minimumArea(new int[][]{{1,0},{0,0}}));
+        LongestSubarrayOfOnesAfterDeletingOneElement sol=new LongestSubarrayOfOnesAfterDeletingOneElement();
+        System.out.println(sol.longestSubarray(new int[]{1,1,0,1}));
+        System.out.println(sol.longestSubarray(new int[]{0,1,1,1,0,1,1,0,1}));
+        System.out.println(sol.longestSubarray(new int[]{1,1,1}));
     }
 }
