b8f194f22eaeb2475bc42b1e02fdc31cc9e08a4b
Srinjoy Sur <srinjoy_sur@epam.com>
2025-09-08 08:55:34 +0530

    update: Main

--- src/main/java/Leetcode/Main.java
+++ src/main/java/Leetcode/Main.java
@@ -4,42 +4,6 @@ import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
-//        LetterCombinationsOfAPhoneNumber sol=new LetterCombinationsOfAPhoneNumber();
-//        System.out.println(sol.letterCombinations("23"));
-//        System.out.println(sol.letterCombinations(""));
-//        System.out.println(sol.letterCombinations("2"));
-//        FourSum fourSum=new FourSum();
-//        System.out.println(fourSum.fourSum(new int[]{1,0,-1,0,-2,2},0));
-//        System.out.println(fourSum.fourSum(new int[]{2,2,2,2,2},8));
-//            FindTheMinimumAreaToCoverAllOnes sol=new FindTheMinimumAreaToCoverAllOnes();
-//            System.out.println(sol.minimumArea(new int[][]{{0,1,0},{1,0,1}}));
-//            System.out.println(sol.minimumArea(new int[][]{{1,0},{0,0}}));
-//        MaximumAreaOfLongestDiagonalRectangle sol=new MaximumAreaOfLongestDiagonalRectangle();
-//        System.out.println(sol.areaOfMaxDiagonal(new int[][]{{9,3},{8,6}}));
-//        System.out.println(sol.areaOfMaxDiagonal(new int[][]{{3,4},{4,3}}));
-//        SortMatrixByDiagonals sol=new SortMatrixByDiagonals();
-//        System.out.println(Arrays.deepToString(sol.sortMatrix(new int[][]{{1, 7, 3}, {9, 8, 2}, {4, 5, 6}})));
-//        System.out.println(Arrays.deepToString(sol.sortMatrix(new int[][]{{0,1},{1,2}})));
-//        AliceAndBobPlayingFlowerGame sol=new AliceAndBobPlayingFlowerGame();
-//        System.out.println(sol.flowerGame(3,2));
-//        System.out.println(sol.flowerGame(1,1));
-//        MaximumAveragePassRatio sol=new MaximumAveragePassRatio();
-//        System.out.println(sol.maxAverageRatio(new int[][]{{1,2},{3,5},{2,2}},2));
-//        System.out.println(sol.maxAverageRatio(new int[][]{{2,4},{3,9},{4,5},{2,10}},4));
-//        FindTheNumberOfWaysToPlacePeopleI sol=new FindTheNumberOfWaysToPlacePeopleI();
-//        System.out.println(sol.numberOfPairs(new int[][]{{1,1},{2,2},{3,3}}));
-//        System.out.println(sol.numberOfPairs(new int[][]{{6,2},{4,4},{2,6}}));
-//        System.out.println(sol.numberOfPairs(new int[][]{{3,1},{1,3},{1,1}}));
-//        FindClosestPerson sol=new FindClosestPerson();
-//        System.out.println(sol.findClosest(2,7,4));
-//        System.out.println(sol.findClosest(2,5,6));
-//        System.out.println(sol.findClosest(1,5,3));
-//        MinimumOperationsToMakeTheIntegerZero sol=new MinimumOperationsToMakeTheIntegerZero();
-//        System.out.println(sol.makeTheIntegerZero(3,-2));
-//        System.out.println(sol.makeTheIntegerZero(5,7));
-        FindNUniqueIntegersSumUpToZero sol=new FindNUniqueIntegersSumUpToZero();
-        System.out.println(Arrays.toString(sol.sumZero(5)));
-        System.out.println(Arrays.toString(sol.sumZero(3)));
-        System.out.println(Arrays.toString(sol.sumZero(1)));
+
     }
 }
