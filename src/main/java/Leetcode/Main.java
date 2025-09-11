29c3e25e254e33100705d2aa912dfbcdad7b9e4e
Srinjoy Sur <srinjoy_sur@epam.com>
2025-09-11 09:01:06 +0530

    feat: SortVowelsInAString

--- src/main/java/Leetcode/Main.java
+++ src/main/java/Leetcode/Main.java
@@ -1,9 +1,10 @@
 package Leetcode;
 
-import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
-
+        SortVowelsInAString sol=new SortVowelsInAString();
+        System.out.println(sol.sortVowels("lEetcOde"));
+        System.out.println(sol.sortVowels("lYmpH"));
     }
 }
