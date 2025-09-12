68e79e99d2ee4d0a271d64c689b3b8c033542e4a
Srinjoy Sur <srinjoy_sur@epam.com>
2025-09-12 08:47:21 +0530

    feat: VowelsGameInAString

---
+++ src/main/java/Leetcode/VowelsGameInAString.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+public class VowelsGameInAString {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++)
+            if ((0x104111 >> (s.charAt(i) - 97) & 1) != 0)
+                return true;
+        return false;
+    }
+}
