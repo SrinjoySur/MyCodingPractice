d10c018b1461dc9d10c26d318c0bec4ec24214c5
Srinjoy Sur <srinjoy_sur@epam.com>
2025-10-02 11:54:29 +0530

    feat: WaterBottlesII

---
+++ src/main/java/Leetcode/WaterBottlesII.java
@@ -0,0 +1,13 @@
+package Leetcode;
+
+public class WaterBottlesII {
+    public int maxBottlesDrunk(int numBottles, int x) {
+        int ans = numBottles;
+        while (numBottles >= x) {
+            numBottles -= x - 1;
+            x++;
+            ans++;
+        }
+        return ans;
+    }
+}
