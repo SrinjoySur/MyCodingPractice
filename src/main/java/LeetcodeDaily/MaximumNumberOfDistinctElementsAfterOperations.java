d7413d9ab41710c2d27af878c546f4d3d637bdc7
Srinjoy Sur <srinjoy_sur@epam.com>
2025-10-18 19:51:15 +0530

    feat: MaximumNumberOfDistinctElementsAfterOperations

---
+++ src/main/java/LeetcodeDaily/MaximumNumberOfDistinctElementsAfterOperations.java
@@ -0,0 +1,19 @@
+package LeetcodeDaily;
+
+import java.util.Arrays;
+
+public class MaximumNumberOfDistinctElementsAfterOperations {
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 0, prev = (int)-1e9;
+
+        for (int x : nums) {
+            int l = Math.max(x - k, prev + 1);
+            if (l <= x + k) {
+                prev = l;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
