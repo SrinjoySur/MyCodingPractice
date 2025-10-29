83422b8fcb3d2b5658020a99edc2a5af1ac4f15c
Srinjoy Sur <srinjoy_sur@epam.com>
2025-10-29 09:08:37 +0530

    feat: SmallestNumberWithAllSetBits

---
+++ src/main/java/LeetcodeDaily/SmallestNumberWithAllSetBits.java
@@ -0,0 +1,11 @@
+package LeetcodeDaily;
+
+public class SmallestNumberWithAllSetBits {
+    public int smallestNumber(int n) {
+        for (int i = 0; i <= n; i++) {
+            int z = 1 << i;
+            if (z > n) return z - 1;
+        }
+        return 0;
+    }
+}
