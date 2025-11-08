db69e2363be52d9413a2e30e6415db4feffb1914
Srinjoy Sur <srinjoy_sur@epam.com>
2025-11-08 21:28:45 +0530

    feat: MinimumOneBitOperationsToMakeIntegersZero

---
+++ src/main/java/LeetcodeDaily/MinimumOneBitOperationsToMakeIntegersZero.java
@@ -0,0 +1,12 @@
+package LeetcodeDaily;
+
+public class MinimumOneBitOperationsToMakeIntegersZero {
+    public int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n > 0) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+}
