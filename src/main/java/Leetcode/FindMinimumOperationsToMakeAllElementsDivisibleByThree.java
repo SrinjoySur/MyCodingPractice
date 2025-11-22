06a3baf7b223e183bb96e2f915b370f4870884e3
Srinjoy Sur <srinjoysur@gmail.com>
2025-11-22 20:04:36 +0530

    feat: FindMinimumOperationsToMakeAllElementsDivisibleByThree

---
+++ src/main/java/Leetcode/FindMinimumOperationsToMakeAllElementsDivisibleByThree.java
@@ -0,0 +1,11 @@
+package Leetcode;
+
+public class FindMinimumOperationsToMakeAllElementsDivisibleByThree {
+    public int minimumOperations(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            if (num % 3 != 0) res++;
+        }
+        return res;
+    }
+}
