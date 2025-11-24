ea523931e5fd2565f58ba80d29124d04cc578f71
Srinjoy Sur <srinjoy_sur@epam.com>
2025-11-24 09:20:51 +0530

    feat: BinaryPrefixDivisibleByFive

---
+++ src/main/java/LeetcodeDaily/BinaryPrefixDivisibleByFive.java
@@ -0,0 +1,18 @@
+package LeetcodeDaily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryPrefixDivisibleByFive {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> res = new ArrayList<>();
+        int val = 0;
+
+        for (int n : nums) {
+            val = ((val << 1) + n) % 5;
+            res.add(val == 0);
+        }
+
+        return res;
+    }
+}
