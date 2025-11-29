b2577479b5d071a0a02b429e48330d9bdedae6c1
Srinjoy Sur <srinjoy_sur@epam.com>
2025-11-29 09:52:51 +0530

    feat: MinimumOperationsToMakeArraySumDivisibleByK

---
+++ src/main/java/LeetcodeDaily/MinimumOperationsToMakeArraySumDivisibleByK.java
@@ -0,0 +1,11 @@
+package LeetcodeDaily;
+
+public class MinimumOperationsToMakeArraySumDivisibleByK {
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum+=nums[i];
+        }
+        return sum%k;
+    }
+}
