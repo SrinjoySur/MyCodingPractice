e30a124e665ff2120a13072a3cfab21950864fae
Srinjoy Sur <srinjoy_sur@epam.com>
2025-12-04 10:53:03 +0530

    feat: CountCollisionsOnARoad

---
+++ src/main/java/LeetcodeDaily/CountCollisionsOnARoad.java
@@ -0,0 +1,13 @@
+package LeetcodeDaily;
+
+public class CountCollisionsOnARoad {
+    public int countCollisions(String directions) {
+        int n = directions.length(), ans = 0, l = 0, r = n - 1;
+        while(l < n && directions.charAt(l) == 'L') l++;
+        while(r >= l && directions.charAt(r) == 'R') r--;
+        for(int i = l ; i <= r; i++) {
+            if(directions.charAt(i) != 'S') ans++;
+        }
+        return ans;
+    }
+}
