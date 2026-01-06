d0ae5b08baec5d5d77badcdf873aa550354adbad
Srinjoy Sur <srinjoy_sur@epam.com>
2026-01-06 09:50:04 +0530

    feat: MaximumLevelSumOfABinaryTree

---
+++ src/main/java/LeetcodeDaily/MaximumLevelSumOfABinaryTree.java
@@ -0,0 +1,36 @@
+package LeetcodeDaily;
+
+import java.util.LinkedList;
+
+public class MaximumLevelSumOfABinaryTree {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int level = 1;
+        int answerLevel = 1;
+        long maxSum = Long.MIN_VALUE;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long levelSum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelSum += node.val;
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                answerLevel = level;
+            }
+
+            level++;
+        }
+
+        return answerLevel;
+    }
+}
