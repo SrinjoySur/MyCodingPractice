20afa3137a579468cd92b28a47c66749c3d884ea
Srinjoy Sur <srinjoysur@gmail.com>
2026-01-25 16:24:27 +0530

    feat: MinimumDifferenceBetweenHighestAndLowestOfKScores

---
+++ src/main/java/LeetcodeDaily/MinimumDifferenceBetweenHighestAndLowestOfKScores.java
@@ -0,0 +1,16 @@
+package LeetcodeDaily;
+
+import java.util.Arrays;
+
+public class MinimumDifferenceBetweenHighestAndLowestOfKScores {
+    public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = nums[k - 1] - nums[0];
+        for (int i = 0; i + k <= n; i++) {
+            ans = Math.min(ans, nums[i + k - 1] - nums[i]);
+        }
+        return ans;
+    }
+}
+}
