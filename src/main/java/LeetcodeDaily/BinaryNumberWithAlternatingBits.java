c283de3734d1ad29cd342abdb3705dd9d908d1e4
Srinjoy Sur <srinjoy_sur@epam.com>
2026-02-18 10:18:12 +0530

    feat: BinaryNumberWithAlternatingBits

---
+++ src/main/java/LeetcodeDaily/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,8 @@
+package LeetcodeDaily;
+
+public class BinaryNumberWithAlternatingBits {
+    public boolean hasAlternatingBits(int n) {
+        int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+}
