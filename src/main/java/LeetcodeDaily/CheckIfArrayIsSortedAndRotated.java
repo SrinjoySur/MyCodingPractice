17df700e79c4c507bbd702699308e7ff2ed24875
Srinjoy Sur <srinjoy_sur@epam.com>
2026-05-23 20:35:02 +0530

    feat: CheckIfArrayIsSortedAndRotated

---
+++ src/main/java/LeetcodeDaily/CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,17 @@
+package LeetcodeDaily;
+
+public class CheckIfArrayIsSortedAndRotated {
+    public boolean check(int[] nums) {
+        boolean fault = false;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                if (fault) return false;
+                fault = true;
+            }
+        }
+
+        return true;
+    }
+}
