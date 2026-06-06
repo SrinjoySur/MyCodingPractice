33730b376c7b2a9bbfe7c73902e9a78ce29cf2ed
Srinjoy Sur <srinjoy_sur@epam.com>
2026-06-06 10:11:57 +0530

    feat: LeftAndRightSumDifferences

---
+++ src/main/java/LeetcodeDaily/LeftAndRightSumDifferences.java
@@ -0,0 +1,22 @@
+package LeetcodeDaily;
+
+public class LeftAndRightSumDifferences {
+    public int[] leftRightDifference(int[] nums) {
+        int rightSum = 0;
+
+        for (int num : nums) {
+            rightSum += num;
+        }
+
+        int leftSum = 0;
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            result[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
+        }
+
+        return result;
+    }
+}
