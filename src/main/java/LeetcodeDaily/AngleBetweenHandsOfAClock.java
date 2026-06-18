ff29d6eab007d09470247c8741613a89e0aceb44
Srinjoy Sur <srinjoy_sur@epam.com>
2026-06-18 09:10:09 +0530

    feat: AngleBetweenHandsOfAClock

---
+++ src/main/java/LeetcodeDaily/AngleBetweenHandsOfAClock.java
@@ -0,0 +1,9 @@
+package LeetcodeDaily;
+
+public class AngleBetweenHandsOfAClock {
+    public double angleClock(int hour, int minutes) {
+        double x = hour + minutes / 60.0;
+        double diff = (11.0 * x) % 12.0;
+        return Math.min(diff, 12.0 - diff) * 30.0;
+    }
+}
