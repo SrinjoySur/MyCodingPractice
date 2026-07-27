95d76a527ec1e1ef8fda61d887bebe9a9e3841a7
Srinjoy Sur <srinjoysur@gmail.com>
2026-07-27 10:05:41 +0530

    feat: MaximumProductOfTwoElementsInAnArray

---
+++ src/main/java/LeetcodeDaily/MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,19 @@
+package LeetcodeDaily;
+
+public class MaximumProductOfTwoElementsInAnArray {
+    public int maxProduct(int[] nums) {
+        int max1 = 1, max2 = 1;
+
+        for(int ele : nums){
+            if(max1 <= ele){
+                max2 = max1;
+                max1 = ele;
+            }
+            else if(max2 <= ele){
+                max2 = ele;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
