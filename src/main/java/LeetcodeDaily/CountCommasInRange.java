99088df097e48810bd79ab8820737c586e6ad35b
Srinjoy Sur <srinjoy_sur@epam.com>
2026-09-08 15:44:46 +0530

    feat: CountCommasInRange

---
+++ src/main/java/LeetcodeDaily/CountCommasInRange.java
@@ -0,0 +1,15 @@
+package LeetcodeDaily;
+
+public class CountCommasInRange {
+    public int countCommas(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i >= 1000) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
